3e4934194ac24c186ad43af86353bde0f84e6edb
imran.aliyev@accessholding.com <imran.aliyev@gmail.com>
2021-01-07 20:09:29 +0100

    Using of Mutex to acquire exclusive lock to shared resource

--- main.go
+++ main.go
@@ -14,27 +14,27 @@ func main() {
 	rand.Seed(time.Now().UnixNano())
 
 	wg := &sync.WaitGroup{}
+	m := &sync.Mutex{}
 
-	for i := 0; i < 10; i++ {
+	for i := 0; i < 20; i++ {
 		id := rand.Intn(10) + 1
 		wg.Add(2) // adding number of tasks for wait
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := getFromCache(id); ok {
+		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+			if b, ok := getFromCache(id, m); ok {
 				fmt.Println("from cache")
 				fmt.Println(b)
 
 			}
 			wg.Done() // signal that task was done
-		}(id, wg)
+		}(id, wg, m)
 
-		go func(id int, wg *sync.WaitGroup) {
-			if b, ok := getFromDb(id); ok {
+		go func(id int, wg *sync.WaitGroup, m *sync.Mutex) {
+			if b, ok := getFromDb(id, m); ok {
 				fmt.Println("from db")
 				fmt.Println(b)
-
 			}
 			wg.Done()
-		}(id, wg)
+		}(id, wg, m)
 
 		time.Sleep(150 * time.Millisecond)
 	}
@@ -42,16 +42,20 @@ func main() {
 	wg.Wait() // wait for all routines are done
 }
 
-func getFromCache(id int) (Book, bool) {
+func getFromCache(id int, m *sync.Mutex) (Book, bool) {
+	m.Lock()
 	b, ok := cache[id]
+	m.Unlock()
 	return b, ok
 }
 
-func getFromDb(id int) (Book, bool) {
+func getFromDb(id int, m *sync.Mutex) (Book, bool) {
 	time.Sleep(100 * time.Millisecond)
 	for _, b := range books {
 		if b.ID == id {
+			m.Lock()
 			cache[id] = b
+			m.Unlock()
 			return b, true
 		}
 	}
